951aac795c4ad7993f7cfee2d50b2a3e3e0b4992
KillianFischer <killian.fischer1@gmail.com>
2025-01-02 18:20:26 +0100

    first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-^uz^)+-u2fmbop7o!j6$96qevew+2aq9zlr6ord1w1qn$+9eyf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
