1975b7ca4546a0ea21db046e32cbe1fdcb1603af
KillianFischer <killian.fischer1@gmail.com>
2025-01-12 11:47:02 +0100

    Changes

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -92,12 +92,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "mysite.wsgi.application"
 
-# Database configuration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    )
 }
 
 
