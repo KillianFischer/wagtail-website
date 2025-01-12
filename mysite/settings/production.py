df14787e83a41852bd1b6a1aba4cac40602058bc
KillianFischer <killian.fischer1@gmail.com>
2025-01-12 12:27:44 +0100

    Changes

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True  # Temporarily set to True
 ALLOWED_HOSTS = ['plankton-app-qnkmw.ondigitalocean.app']
 CSRF_TRUSTED_ORIGINS = ['https://plankton-app-qnkmw.ondigitalocean.app']
 
@@ -22,12 +22,46 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # HSTS settings
-SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
 # Wagtail settings
 WAGTAILADMIN_BASE_URL = 'https://plankton-app-qnkmw.ondigitalocean.app'
+
+# Detailed logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'wagtail': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
 
 try:
     from .local import *
