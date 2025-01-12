8b0e410c0de63ebd6fe9f0d1859076b05422b227
KillianFischer <killian.fischer1@gmail.com>
2025-01-12 11:14:36 +0100

    Changes

---
+++ home/storage.py
@@ -0,0 +1,20 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
+
+class CustomS3Storage(S3Boto3Storage):
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    location = 'original_images'
+    file_overwrite = False
+    default_acl = 'public-read'
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
+    url_protocol = 'https:'
+    endpoint_url = settings.AWS_S3_ENDPOINT_URL
+
+class RenditionS3Storage(S3Boto3Storage):
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    location = 'images'
+    file_overwrite = False
+    default_acl = 'public-read'
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
+    url_protocol = 'https:'
+    endpoint_url = settings.AWS_S3_ENDPOINT_URL 
